e1c3af308560fed401e1a75a5f0c3b282dd3445e
praddumnyadav <praddumn.kumar@oodlestechnologies.com>
2018-10-30 15:26:46 +0530

    modification for setting initial supply

--- contracts/CZToken.sol
+++ contracts/CZToken.sol
@@ -1,50 +1,86 @@
 pragma solidity ^0.4.24;
+
 import "zos-lib/contracts/migrations/Migratable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
-// import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+import "openzeppelin-zos/contracts/lifecycle/Pausable.sol";
+import "openzeppelin-zos/contracts/ownership/Ownable.sol";
+import "openzeppelin-zos/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-zos/contracts/token/ERC20/StandardBurnableToken.sol";
 
 
+contract CZToken is Migratable, Pausable, StandardBurnableToken {
 
-contract CZToken is Migratable, ERC20, PausableToken, BurnableToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
 
-  // ERC20 BASIC DATA
+    function initialize(address _sender, string _name, string _symbol, uint8 _decimals) 
+    public 
+    isInitializer("CZToken", "0.1") 
+    {
+        require(_sender != address(0), "Null address not allowed.");
+        require(bytes(_name).length > 0, "Name should be provided.");
+        require(bytes(_name).length > 0, "Symbol should be provided.");
+        require(_decimals >= 0, "Decimals should be a non-negative integer quantity.");
 
-  string public  name;
-  string public  symbol;
-  uint8 public  decimals;
-  uint256 public INITIAL_SUPPLY;
-  uint256 private totalSupply_;
-  uint256 public counter;
-  mapping(uint256 => address) public history;
+      // setting the owner's address
+        Pausable.initialize(_sender);
 
+        // token parameters initialization
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
 
+        // calculating total supply value
+        uint256 _totalSupplyValue = 1000000000 * (10 ** uint256 (decimals));
+        
+        // setting token's total supply
+        totalSupply_ = _totalSupplyValue;
 
-  function initialize(string _name, string _symbol, uint8 _decimals, uint8 _counter) isInitializer("CZToken", "1.0.0") public {
+        // updating the token balance of the owner
+        balances[_sender] = balances[_sender].add(_totalSupplyValue);
+    }
 
-        name = _name; // 'COINZOOM';
-        symbol = _symbol; // 'ZOOM';
-        decimals = _decimals; // 18;
-        INITIAL_SUPPLY = 1000000000 * (10 ** 18);
-        owner = msg.sender;
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
-        counter = _counter;
+    function() public payable {
+        revert("This contract does not support recieving ether.");
+    }
 
-      }
+    function transfer(address _to, uint256 _value) 
+    public 
+    whenNotPaused 
+    returns (bool) 
+    {
+        return super.transfer(_to, _value);
+    }
 
+    function transferFrom(address _from, address _to, uint256 _value) 
+    public 
+    whenNotPaused 
+    returns (bool) 
+    {
+        return super.transferFrom(_from, _to, _value);
+    }
 
+    function approve(address _spender, uint256 _value) 
+    public 
+    whenNotPaused 
+    returns (bool) 
+    {
+        return super.approve(_spender, _value);
+    }
 
-      function increment() public {
-        counter += 1;
-        history[counter] = msg.sender;
-   }
+    function increaseApproval(address _spender, uint _addedValue) 
+    public 
+    whenNotPaused 
+    returns (bool success) 
+    {
+        return super.increaseApproval(_spender, _addedValue);
+    }
 
-      function incrementByTwo() public {
-        counter += 2;
-        history[counter] = msg.sender;
-   }
-   
-
+    function decreaseApproval(address _spender, uint _subtractedValue) 
+    public 
+    whenNotPaused 
+    returns (bool success) 
+    {
+        return super.decreaseApproval(_spender, _subtractedValue);
+    }
 }
